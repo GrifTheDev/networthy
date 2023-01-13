2f1a6229529f0a5178a8392eaa0a2af204b1be6d
GrifTheDev <105976474+Kontributor1@users.noreply.github.com>
2023-01-13 19:01:21 +0100

    Inital Commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import { Command } from "commander";
+const program: Command = new Command();
+
+program
+  .name("networthy")
+  .description("A CLI for HTTP requests, made in Typescript.")
+  .version("0.1.0");
+
+program.command('GET')
+    .description("Make a GET request to the specified URL.")
+    .argument("<URL>", "The URL to which you want to make the GET request.")
+    .action((str) => {
+        console.log(str)
+    })
+
+program.parse();
